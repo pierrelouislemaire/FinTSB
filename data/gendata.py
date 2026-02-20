93d70b058c1893fa33c67b58f9cd541eedd6f851
pierrelouislemaire <pierre-louis.lemaire@mila.quebec>
2026-02-20 15:44:12 -0500

    data generation + preprocessing + visualization

--- data/gendata.py
+++ data/gendata.py
@@ -5,7 +5,7 @@ patterns = ["fluctuation", "rise", "fall", "extreme"]
 merged_data = []
 
 for pattern in patterns:
-    all_files = glob.glob(f"./FinTSB_cn/{pattern}/dataset_*.pkl")
+    all_files = glob.glob(f"./data/FinTSB_cn/{pattern}/dataset_*.pkl")
     for source_id, file_path in enumerate(sorted(all_files)):
         df = pd.read_pickle(file_path)
     
@@ -20,4 +20,4 @@ for pattern in patterns:
 print(len(merged_data))
 final_df = pd.concat(merged_data)
 print(final_df.shape)
-final_df.to_pickle("./FinTSB_cn/merged_dataset.pkl")
+final_df.to_pickle("./data/FinTSB_cn/merged_dataset.pkl")
